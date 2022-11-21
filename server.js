04a5bf055a5d152a16e5ede76434656b407df532
unknown <E:\The Bat!>
2022-11-21 14:34:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,17 +12,3 @@ db.then(() => {
   console.log(`Server not running. Error message: ${err.message}`);
   process.exit(1);
 });
-
-// const start = async () => {
-//   try {
-//     mongoose.connect(HOST_DB);
-//     app.listen(PORT, () => {
-//       console.log(`Server running. Use our API on port: ${PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("Failed to start server with error: ", error.message);
-//     process.exit(1);
-//   }
-// };
-
-// start();
