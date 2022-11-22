2131087bca7f631f1d57d018ecbe4645a919f116
unknown <4ndrey.tkachenko@gmail.com>
2022-11-22 14:36:15 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const contactsRouter = require("./routes/api");
+const authRouter = require("./routes/api/authRouter");
+
+const app = express();
+
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.use("/api/contacts", contactsRouter);
+app.use("/api/users", authRouter);
+app.use("/", (req, res) => {
+  res.status(404).json({message: "Not found!"});
+});
+
+app.use((err, _, res, __) => {
+  const {status = 500, message = "Internal Server Error"} = err;
+  res.status(status).json(message);
+});
+
+module.exports = app;
